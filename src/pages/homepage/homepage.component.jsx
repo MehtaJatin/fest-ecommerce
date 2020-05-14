f11e57fc7c0c5e85d931229cb8c0a44518c61d33
Jatin <mehtajatin47.jm@gmail.com>
2020-05-15 01:06:04 +0530

    broken our homepage into smaller ocmponent

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Menu from '../../components/menu/menu-component.jsx';
+const HomePage = () => (
+    <div className='homepage'>
+       <Menu />
+    </div>
+);
+
+export default HomePage;
